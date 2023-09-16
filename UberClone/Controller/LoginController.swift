8722f735dfeee8f1e34923ada75d018f6dc54c91
David Goren <dgoren@hotmail.com>
2023-09-16 20:02:07 -0300

    adding the first label to the project

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class LoginController: UIViewController {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0)
+        ])
     }
-
-
 }
