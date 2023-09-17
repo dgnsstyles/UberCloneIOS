c1fc12beac1ac232b0c72849dbcd571c86273a53
David Goren <dgoren@hotmail.com>
2023-09-17 14:54:41 -0300

    added the AuthButton and LocationHandler

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -7,25 +7,86 @@
 
 import UIKit
 
+struct ConstanstsLoginController {
+    static let UberLoginBackgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+    enum LoginUberEmailContainerView {
+        static let textLabelUber = "UBER"
+        static let fontFamilyUber =  UIFont(name: "Avenir-Light", size: 36)
+        static let textColorUber = UIColor(white: 1, alpha: 0.8)
+    }
+   
+    
+}
+
 class LoginController: UIViewController {
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "UBER"
-        label.font = UIFont(name: "Avenir-Light", size: 36)
-        label.textColor = UIColor(white: 1, alpha: 0.8)
+        label.text = ConstanstsLoginController.LoginUberEmailContainerView.textLabelUber
+        label.font = ConstanstsLoginController.LoginUberEmailContainerView.fontFamilyUber
+        label.textColor = ConstanstsLoginController.LoginUberEmailContainerView.textColorUber
         return label
+    }()
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Email",
+                                       isSecureTextEntry: false)
+    }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password",
+                                       isSecureTextEntry: true)
+    }()
+    private let loginButton: AuthButton = {
+        let button = AuthButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+       configureUI()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .backgroundColor
         
         view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0)
-        ])
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 24
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 40, paddingLeft: 16,
+                     paddingRight: 16)
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
--- UberClone/View/AuthButton.swift
+++ UberClone/View/AuthButton.swift
@@ -5,4 +5,23 @@
 //  Created by David Goren on 17-09-23.
 //
 
-import Foundation
+import UIKit
+
+class AuthButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpUI()
+    }
+    
+    func setUpUI() {
+        layer.cornerRadius = 5
+        backgroundColor = .mainBlueTint
+        setTitleColor(.white, for: .normal)
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
